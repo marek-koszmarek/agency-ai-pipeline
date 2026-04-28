7ed8134d2025e7ab1df2e99edda2e05800ab1c1e
marek-koszmarek <marek@luzno.agency>
2026-04-28 18:01:55 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
+  },
+  // Increase body parser limit for API routes
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb',
+    },
+  },
+};
 export default nextConfig;
